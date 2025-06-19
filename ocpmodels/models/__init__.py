183fd2665a63d31c7c613253ac1a210c59cad885
Swaroop <53393182+swaroop2005@users.noreply.github.com>
2025-06-20 00:07:47 +0530

    Update __init__.py

--- ocpmodels/models/__init__.py
+++ ocpmodels/models/__init__.py
@@ -4,10 +4,5 @@
 # LICENSE file in the root directory of this source tree.
 
 from .base import BaseModel
-from .cgcnn import CGCNN
-from .dimenet import DimeNetWrap as DimeNet
-from .dimenet_plus_plus import DimeNetPlusPlusWrap as DimeNetPlusPlus
 from .forcenet import ForceNet
 from .gemnet.gemnet import GemNetT
-from .schnet import SchNetWrap as SchNet
-from .spinconv import spinconv
